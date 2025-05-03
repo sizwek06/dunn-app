7fa5403c99f735464f1f91a27fd68fdd6d7ca4f7
Sizwe Khathi <57193662+sizwek06@users.noreply.github.com>
2025-05-03 05:53:23 +0200

    Add theming items

--- dun/dun/Theme/TodoStrings.swift
+++ dun/dun/Theme/TodoStrings.swift
@@ -10,8 +10,15 @@ import UIKit
 
 struct TodoStrings {
     
+    static let weatherTitle = "Today's Weather"
+    static let currentTemperature = "Temp: "
+    static let sunsetTime = "Sunset: "
+    static let sunriseTime = "| Sunrise: "
+    
+    
     static let todoListTitle = "To-Do List"
-    static let todoListButtonTitle = "Add Todo Item"
+    static let todoItemTitlePlaceHolder = "Cereal"
+    static let todoItemDescrPlaceHolder = "Get cereal ingredients"
     static let noTodoItemsListText = "No Todo Items"
     
     static let settingsViewTitle = "Settings"
@@ -30,6 +37,9 @@ struct TodoStrings {
     
     static let userDefaultBiometricsKey = "faceID"
     static let useFaceIDText = "Use FaceID"
+    
+    static let sfProRounded = "SFProRounded-Bold"
+    static let sfProRegular = "SF-ProText-Regular"
 }
 
 enum BiometricError: LocalizedError {
